d40896b28e067bfab63474ab0988f08117cce60f
John Lawless <jlawless@iastate.edu>
2021-04-10 10:45:19 -0500

    Prettifying first step

--- HUD scenes/Shop.gd
+++ HUD scenes/Shop.gd
@@ -19,7 +19,7 @@ func _ready():
 func _on_Tower1_pressed():
 	$ShopInfo/Name.text = "Water Tower"
 	$ShopInfo/Description.text = "Tower1 description"
-	$ShopInfo/Cost.text = "Tower1 cost"
+	$ShopInfo/Cost.text = "Cost: " + String(get_parent().WATER_COST)
 	$ShopInfo/Tower.play("Blue")
 	selected = "Blue"
 	
@@ -28,14 +28,14 @@ func _on_Tower1_pressed():
 func _on_Tower2_pressed():
 	$ShopInfo/Name.text = "Earth Tower"
 	$ShopInfo/Description.text = "Tower2 description"
-	$ShopInfo/Cost.text = "Tower2 cost"
+	$ShopInfo/Cost.text = "Cost: " + String(get_parent().EARTH_COST)
 	$ShopInfo/Tower.play("Brown")
 	selected = "Brown"
 
 func _on_Tower3_pressed():
 	$ShopInfo/Name.text = "Lightning Tower"
 	$ShopInfo/Description.text = "Tower3 description"
-	$ShopInfo/Cost.text = "Tower3 cost"
+	$ShopInfo/Cost.text = "Cost: " + String(get_parent().LIGHTNING_COST)
 	$ShopInfo/Tower.play("Yellow")
 	selected = "Yellow"
 
@@ -43,7 +43,7 @@ func _on_Tower3_pressed():
 func _on_Tower4_pressed():
 	$ShopInfo/Name.text = "Air Tower"
 	$ShopInfo/Description.text = "Tower4 description"
-	$ShopInfo/Cost.text = "Tower4 cost"
+	$ShopInfo/Cost.text = "Cost: " + String(get_parent().AIR_COST)
 	$ShopInfo/Tower.play("Grey")
 	selected = "Grey"
 
@@ -51,7 +51,7 @@ func _on_Tower4_pressed():
 func _on_Tower5_pressed():
 	$ShopInfo/Name.text = "Fire Tower"
 	$ShopInfo/Description.text = "Tower5 description"
-	$ShopInfo/Cost.text = "Tower5 cost"
+	$ShopInfo/Cost.text = "Cost: " + String(get_parent().FIRE_COST)
 	$ShopInfo/Tower.play("Red")
 	selected = "Red"
 
