febb4338a0866c51909defd5f0ffa4d2bbbba7c5
jmriesen <jmriesen@iastate.edu>
2021-04-10 08:45:29 -0500

    modified it so that it only places towers on the green

--- Field/Field.gd
+++ Field/Field.gd
@@ -16,8 +16,9 @@ func _input(event):
 		if event is InputEventMouseButton and event.is_pressed():
 			var mouse = get_viewport().get_mouse_position()
 			var map_cords = $TileMap.world_to_map(mouse)
-			if (0 < map_cords.x  && map_cords.x < grid_width
-			 && 0 < map_cords.y  && map_cords.y < grid_hight ):
+			#if (0 < map_cords.x  && map_cords.x < grid_width
+			# && 0 < map_cords.y  && map_cords.y < grid_hight ):
+			if 0 == $TileMap.get_cellv(map_cords):
 				var tower = Tower.instance()
 				tower.get_node("TowerAnim").play($Shop.selected)
 				tower.position = mouse
