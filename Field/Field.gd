561625b5c80b2a52df9bf0a0bde623e7a0e2ed5c
nsagers <nsgers@gmail.com>
2021-04-10 09:35:04 -0500

    Towers cost money now :(

--- Field/Field.gd
+++ Field/Field.gd
@@ -11,6 +11,12 @@ const grid_width =22
 const grid_hight =20
 var started = false
 
+const FIRE_COST = 20
+const WATER_COST = 15
+const LIGHTNING_COST = 22
+const EARTH_COST = 8
+const AIR_COST = 100
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	randomize()
@@ -24,10 +30,13 @@ func _input(event):
 			if 0 == $TileMap.get_cellv(map_cords):
 				var tower_type = FireTower
 				if $Shop.selected == "Red":
+					get_node("MoneyLabel").cash = get_node("MoneyLabel").cash - FIRE_COST
 					tower_type = FireTower
 				if $Shop.selected == "Blue":
+					get_node("MoneyLabel").cash = get_node("MoneyLabel").cash - WATER_COST
 					tower_type = Tower
 				if $Shop.selected == "Grey":
+					get_node("MoneyLabel").cash = get_node("MoneyLabel").cash - AIR_COST
 					tower_type = AirTower
 				
 				var tower = tower_type.instance()
