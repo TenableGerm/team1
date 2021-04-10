d2713c887c45bcb04d2a8f693b67dd16466e2edd
unknown <gavingrg649@gmail.com>
2021-04-10 03:15:09 -0500

    AnimButtons

--- HUD scenes/Shop.gd
+++ HUD scenes/Shop.gd
@@ -17,30 +17,35 @@ func _ready():
 
 
 func _on_Tower1_pressed():
-	$ShopInfo/Name.text = "Tower1 name"
+	$ShopInfo/Name.text = "Water Tower"
 	$ShopInfo/Description.text = "Tower1 description"
 	$ShopInfo/Cost.text = "Tower1 cost"
+	$ShopInfo/Tower.play("Blue")
+	
 
 
 func _on_Tower2_pressed():
-	$ShopInfo/Name.text = "Tower2 name"
+	$ShopInfo/Name.text = "Earth Tower"
 	$ShopInfo/Description.text = "Tower2 description"
 	$ShopInfo/Cost.text = "Tower2 cost"
-
+	$ShopInfo/Tower.play("Brown")
 
 func _on_Tower3_pressed():
-	$ShopInfo/Name.text = "Tower3 name"
+	$ShopInfo/Name.text = "Lightning Tower"
 	$ShopInfo/Description.text = "Tower3 description"
 	$ShopInfo/Cost.text = "Tower3 cost"
+	$ShopInfo/Tower.play("Yellow")
 
 
 func _on_Tower4_pressed():
 	$ShopInfo/Name.text = "Tower4 name"
 	$ShopInfo/Description.text = "Tower4 description"
 	$ShopInfo/Cost.text = "Tower4 cost"
+	$ShopInfo/Tower.play("Grey")
 
 
 func _on_Tower5_pressed():
 	$ShopInfo/Name.text = "Tower5 name"
 	$ShopInfo/Description.text = "Tower5 description"
 	$ShopInfo/Cost.text = "Tower5 cost"
+	$ShopInfo/Tower.play("Red")
