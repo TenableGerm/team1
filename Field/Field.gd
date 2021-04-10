f8030664ef3767992f25c81a99ea6753661c0ebb
unknown <gavingrg649@gmail.com>
2021-04-10 08:38:27 -0500

    FireBullet

--- Field/Field.gd
+++ Field/Field.gd
@@ -2,7 +2,7 @@ extends CanvasLayer
 
 export (PackedScene) var Path
 export (PackedScene) var Tower  
-
+export (PackedScene) var FireTower 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	randomize()
@@ -11,11 +11,18 @@ func _ready():
 func _input(event):
 	if event is InputEventMouseButton and event.is_pressed():
 		var mouse = get_viewport().get_mouse_position()
-		var tower = Tower.instance()
-		tower.get_node("TowerAnim").play($Shop.selected)
-		tower.position = mouse
-		#tower.get_node("Fire").position = mouse
-		add_child(tower)
+		if $Shop.selected == "Blue":
+			var tower = Tower.instance()
+			tower.get_node("TowerAnim").play($Shop.selected)
+			tower.position = mouse
+			#tower.get_node("Fire").position = mouse
+			add_child(tower)
+		if $Shop.selected == "Red":
+			var tower = FireTower.instance()
+			tower.get_node("TowerAnim").play($Shop.selected)
+			tower.position = mouse
+			#tower.get_node("Fire").position = mouse
+			add_child(tower)
 
 
 func _on_EnemyButton_pressed():
