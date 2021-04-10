3e1a85489fc67f18b918f162c31386927d586b7e
John Lawless <jlawless@iastate.edu>
2021-04-10 07:48:33 -0500

    Starting workings of a health bar

--- Field/Pollution.gd
+++ Field/Pollution.gd
@@ -6,13 +6,11 @@ var pollution
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pollution = 0
-	get_parent().get_node("Path2D").get_node("PathFollow2D").get_node("Enemy").connect("damage", self, "pollution_up")
 
 func pollution_up():
 	pollution = pollution + 1
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	
 	text = ("Position: " + str(pollution))
 	show()
